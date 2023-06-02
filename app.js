b4ef16a6221911458f6ced2acb425f09c0689a13
Hafnium780 <97712621+Hafnium780@users.noreply.github.com>
2023-06-02 11:59:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const { Server } = require("socket.io");
 const express = require("express");
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 8080;
+const io = new Server(server);
 
-app.get("/", (req, res) => res.sendFile("client.html", {root: __dirname}));
+app.get("/", (req, res) => res.sendFile(__dirname + "/client.html"));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+io.on("connection", (s) => {
+  console.log("a");
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}!`));
